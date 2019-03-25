9f6eb3a492576e791a121b231e1d9a5746d69213
Guilherme | Web developer <mandeparaguilherme@gmail.com>
2019-03-25 01:40:11 -0300

    Including JPA to persist the objects in database

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,30 @@
 package application;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import domain.Person;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Person P1 = new Person(1, "Carlos da Silva", "carlossilva@gmail.com");
-		Person P2 = new Person(2, "Joaquin Torres", "joaquintorres@gmail.com");
-		Person P3 = new Person(3, "Ana Maria", "anamaria@gmail.com");
+		Person P1 = new Person(null, "Carlos da Silva", "carlossilva@gmail.com");
+		Person P2 = new Person(null, "Joaquin Torres", "joaquintorres@gmail.com");
+		Person P3 = new Person(null, "Ana Maria", "anamaria@gmail.com");
 		
-		System.out.println(P1);
-		System.out.println(P2);
-		System.out.println(P3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("dbjpa");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(P1);
+		em.persist(P2);
+		em.persist(P3);
+		em.getTransaction().commit();
+		
+		System.out.println("finish");
+		em.close();
+		emf.close();
 
 	}
 
